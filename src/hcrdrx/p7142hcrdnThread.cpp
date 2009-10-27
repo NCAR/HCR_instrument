ef54beccbb200a17dd3a2e2e64fd1cc2560ea1bf
Charles Martin <martinc@ucar.edu>
2009-10-27 16:43:36 +0000

    dds structure element name changed.
    git-svn-id: http://svn/svn/hcr/trunk@206 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/hcrdrx/p7142hcrdnThread.cpp
+++ src/hcrdrx/p7142hcrdnThread.cpp
@@ -132,7 +132,7 @@ p7142hcrdnThread::publish(char* buf, int n) {
 
   int len = n;
 
-  ts->data.length(len/2);
+  ts->tsdata.length(len/2);
 
   ts->hskp.gates = _gates;
   ts->hskp.chanId = _chanId;
@@ -144,7 +144,7 @@ p7142hcrdnThread::publish(char* buf, int n) {
 	   // convert to shorts
 	   short* data = (short*)buf;
 	   for (int i = 0; i < n/2; i++) {
-		   ts->data[i] = data[i];
+		   ts->tsdata[i] = data[i];
 		   //std::cout << i << "  " << ts->tsdata[i] << std::endl;
 	   }
    } else {
@@ -161,7 +161,7 @@ p7142hcrdnThread::publish(char* buf, int n) {
 				   // decoding will eventually happen here.
 				   double sum = data[in] + data[in+2*_gates];
 				   double result = (sum/_nsum);
-				   ts->data[out] = (short)result;
+				   ts->tsdata[out] = (short)result;
 				   in++;
 				   out++;
 			   }
