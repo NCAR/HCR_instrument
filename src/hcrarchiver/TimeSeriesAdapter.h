72eb5079387bcc473721758d6821d927e7c0bd46
Chris Burghart <burghart@ucar.edu>
2010-05-20 20:49:37 +0000

    new doxygen comments
    git-svn-id: http://svn/svn/hcr/trunk@246 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/hcrarchiver/TimeSeriesAdapter.h
+++ src/hcrarchiver/TimeSeriesAdapter.h
@@ -20,8 +20,20 @@
 
 class TimeSeriesAdapter {
 public:
+    /**
+     * Convert from IwrfTsPulse to ProfilerDDS::TimeSeries.
+     * @param iwrfPulse the IwrfTsPulse to be converted
+     * @param ddsPulse the destination ProfilerDDS::TimeSeries
+     */
     static void IwrfToDDS(const IwrfTsPulse& iwrfPulse, 
             ProfilerDDS::TimeSeries& ddsPulse);
+    /**
+     * Convert from ProfilerDDS::TimeSeries to IwrfTsPulse.
+     * @param ddsPulse the ProfilerDDS::TimeSeries to be converted
+     * @param iwrfPulse the destination IwrfTsPulse
+     * @param packetSequenceNum the packet sequence number to assign to the
+     *     IwrfTsPulse
+     */
     static void DDSToIwrf(const ProfilerDDS::TimeSeries& ddsPulse,
             IwrfTsPulse& iwrfPulse, si64 packetSequenceNum);
 };
