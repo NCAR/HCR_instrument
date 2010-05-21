c20a8fad9b6156d54d51afc59a4006423ca84f9a
Chris Burghart <burghart@ucar.edu>
2010-05-21 19:54:57 +0000

    In profilertypes.idl, added stuff to RadarDDS::Product housekeeping, and removed some now-redundant information there. Small adjustments were made to existing code to match with these changes.
    git-svn-id: http://svn/svn/hcr/trunk@249 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/hcrarchiver/ProductArchiver.cpp
+++ src/hcrarchiver/ProductArchiver.cpp
@@ -14,6 +14,8 @@
 // Pointer to our singleton instance
 ProductArchiver* ProductArchiver::_theArchiver = 0;
 
+static const double SPEED_OF_LIGHT = 2.99792458e8; // m s-1
+
 
 ProductArchiver::ProductArchiver(DDSSubscriber& subscriber, 
         std::string topicName, std::string dataDir) :
@@ -26,25 +28,43 @@ ProductArchiver::~ProductArchiver() {
 
 void
 ProductArchiver::notify() {
-    RadxRay radxRay;
-    
-//    radxRay.setTime(secs, nanosecs);
-    radxRay.setVolumeNumber(0);
-    radxRay.setSweepNumber(0);
-    radxRay.setCalibIndex(0);
-    radxRay.setSweepMode(Radx::SWEEP_MODE_POINTING);    // HCR is pointing only (for now)
-    radxRay.setPolarizationMode(Radx::POL_MODE_HORIZONTAL);
-    radxRay.setPrfMode(Radx::PRF_MODE_FIXED);
-    radxRay.setFollowMode(Radx::FOLLOW_MODE_NONE);
-//    radxRay.setAzimuthDeg(az);
-//    radxRay.setElevationDeg(el);
-//    radxRay.setFixedAngleDeg(fixedAng);
-    radxRay.setIsIndexed(false);
-    radxRay.setAntennaTransition(false);
-//    radxRay.setNSamples(nsamples);
-//    radxRay.setPulseWidthUsec(pulsewidth);
-//    radxRay.setPrtSec(prt);
-    radxRay.setPrt2Sec(0.0);
-    radxRay.setUnambigRange(); // calculate unambiguous range from PRT
-//    radxRay.setNyquistMps(nyquist);
+    while (RadarDDS::ProductSet* ps = getNextItem()) {
+        // Get the ray housekeeping from the first product in the ProductSet
+        RadarDDS::Product &firstProduct = ps->products[0];
+        
+        // get the time tag, in microseconds since 1970-01-01 00:00:00 UTC
+        long long timetagUsecs = firstProduct.hskp.timetag;
+        
+        // Now build the RadxRay from the ProductSet
+        RadxRay radxRay;
+        
+        time_t rayTimeSecs = timetagUsecs / 1000000;
+        int rayTimeNanos = (timetagUsecs % 1000000) * 1000;
+        radxRay.setTime(rayTimeSecs, rayTimeNanos);
+        radxRay.setVolumeNumber(0);
+        radxRay.setSweepNumber(0);
+        radxRay.setCalibIndex(0);
+        radxRay.setSweepMode(Radx::SWEEP_MODE_POINTING);    // HCR is pointing only (for now)
+        radxRay.setPolarizationMode(Radx::POL_MODE_HORIZONTAL);
+        radxRay.setPrfMode(Radx::PRF_MODE_FIXED);
+        radxRay.setFollowMode(Radx::FOLLOW_MODE_NONE);
+//        radxRay.setAzimuthDeg(az);
+//        radxRay.setElevationDeg(el);
+//        radxRay.setFixedAngleDeg(fixedAng);
+        radxRay.setIsIndexed(false);
+        radxRay.setAntennaTransition(false);
+        radxRay.setNSamples(firstProduct.hskp.samples);
+        // Receiver pulse width in seconds
+        radxRay.setPulseWidthUsec(1.0e6 * firstProduct.hskp.rcvr_pulse_width);
+        radxRay.setPrtSec(firstProduct.hskp.prt1);
+        radxRay.setPrt2Sec(0.0);
+        radxRay.setUnambigRange(); // calculate unambiguous range from PRT
+        float wavelength = SPEED_OF_LIGHT / firstProduct.hskp.tx_cntr_freq;
+        float prf = 1.0 / firstProduct.hskp.prt1;
+        float nyquist = (prf * wavelength) / 4;    // Nyquist max speed
+        radxRay.setNyquistMps(nyquist);
+        
+        // Return the item to the DDSReader
+        returnItem(ps);
+    }
 }
