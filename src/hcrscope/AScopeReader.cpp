e909a2980315c2b2e7eae891f41104e68e330a95
Charles Martin <martinc@ucar.edu>
2010-06-23 05:16:18 +0000

    Created a AScopeReader, derived from QtTSReader. It translates a DDS sample into a AScope::TimeSeries, which is then emitted to the hcrscope. Likewise, it receives the AScope::TimeSeries when hcrscope is finished with them, the allowing the DDS sample to be returned to DDS.
    It all builds, but it has not been tested. There will be some gotchas for certain.
    
    git-svn-id: http://svn/svn/hcr/trunk@347 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

---
+++ src/hcrscope/AScopeReader.cpp
@@ -0,0 +1,56 @@
+
+#include "AScopeReader.h"
+#include <QMetaType>
+
+Q_DECLARE_METATYPE(AScope::TimeSeries)
+
+AScopeReader::AScopeReader(DDSSubscriber& subscriber,
+		std::string topicName,
+		double outputRate):
+		QtTSReader(subscriber, topicName, outputRate)
+{
+	// this are required in order to send structured data types
+	// via a qt signal
+	qRegisterMetaType<AScope::TimeSeries>();
+}
+
+AScopeReader::~AScopeReader() {
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void
+AScopeReader::returnItemSlot(AScope::TimeSeries pItem) {
+	
+	RadarDDS::TimeSeriesSequence* ddsItem = static_cast<RadarDDS::TimeSeriesSequence*>(pItem.handle);
+	assert(ddsItem);
+	
+	TSReader::returnItem(ddsItem);
+}
+
+////////////////////////////////////////////////////////////////////////
+/// Implement DDSReader::notify(), which will be called
+/// whenever new samples are added to the DDSReader available
+/// queue. Send the sample to clients via a signal.
+/// The clients must release the sample via returnItem()
+/// when they are finished with it.
+void AScopeReader::notify(){
+	// a DDS data notification has been received.
+	bool sentOne = false;
+	while (RadarDDS::TimeSeriesSequence* ddsItem = getNextItem()) {
+		// send the sample to our clients
+		if (! sentOne && _capture) {
+			AScope::TimeSeries pItem;
+			pItem.gates    = ddsItem->tsList[0].hskp.gates;
+			pItem.chanId   = ddsItem->chanId;
+			pItem.data     = &ddsItem->tsList[0].data[0];
+			pItem.tsLength = ddsItem->tsList.length();
+			emit newItem(pItem);
+			sentOne = true;
+		} else {
+			returnItem(ddsItem);
+		}
+		// tell timers that we have processed a sample
+		clearCapture();
+	}
+}
---
+++ src/hcrscope/AScopeReader.h
@@ -0,0 +1,51 @@
+#ifndef ASCOPEREADER_H_
+#define ASCOPEREADER_H_
+
+#include "AScope.h"
+#include "QtTSReader.h"
+
+/// A DDS reader for the AScope. It is derived from QtTSReader, and translates
+/// DDS samples to AScope::TimeSeries. It will emit a newTSItemSlot(RadarDDS::TimeSeriesSequence*)
+/// when new data arrives.
+class AScopeReader: public QtTSReader 
+{
+		Q_OBJECT
+public:
+
+	/// Constructor
+	/// @param subscriber The DDS subscriber
+	/// @param topicName The DDS topic name
+	/// @param outputRate The desired maximum sample output rate, in Hz,
+    ///    or 0 for no throttling
+	AScopeReader(DDSSubscriber& subscriber,
+			std::string topicName,
+			double outputRate = 0);
+
+	/// Destructor
+	virtual ~AScopeReader();
+
+signals:
+    /// This signal provides an item that falls within
+    /// the desired bandwidth specification.
+    /// @param pItem A pointer to the item. It must be returned
+    /// via returnItem().
+	void newItem(AScope::TimeSeries pItem);
+
+public slots:
+    /// Use this slot to return an item
+    /// @param pItem the item to be returned.
+    void returnItemSlot(AScope::TimeSeries pItem);
+
+
+protected:
+	/// Re-implemented from super class. This function will be  called
+	/// when new samples are available on the topic. If it is time to
+	/// accept a new sample (_capture is true), then the newItem()
+	/// signal will be emitted for one item. Any remaining available items
+	/// will be returned.
+	virtual void notify();
+
+};
+
+
+#endif /*ASCOPEREADER_H_*/
