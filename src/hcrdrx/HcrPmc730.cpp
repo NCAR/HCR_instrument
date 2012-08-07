19a21c4eaafb24733d927cd63092ee976eef799f
Chris Burghart <burghart@ucar.edu>
2012-08-07 17:14:20 +0000

    o updated enumerated names for the digital in and out lines o removed unused method _sendDifferentialSignal()
    git-svn-id: http://svn/svn/hcr/trunk@593 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/hcrdrx/HcrPmc730.cpp
+++ src/hcrdrx/HcrPmc730.cpp
@@ -28,48 +28,43 @@ bool HcrPmc730::_DoSimulate = false;
 HcrPmc730 * HcrPmc730::_theHcrPmc730 = 0;
 
 HcrPmc730::HcrPmc730() : Pmc730(_DoSimulate ? -1 : 0) {
-	// Get the time of day of instantiation (in seconds). This allows us to
-    // return a reasonable pulse count if we're simulating.
-	struct timeval startTv;
-	gettimeofday(&startTv, NULL);
-	_startTimeOfDay = startTv.tv_sec + 1.0e-6 * startTv.tv_usec;
-
     // For HCR, DIO lines 0-7 are used for input, 8-15 for output
     setDioDirection0_7(DIO_INPUT);
     setDioDirection8_15(DIO_OUTPUT);
     // Verify that our defined input DIO lines are all actually set for input
-    if (getDioDirection(_HCR_DIN_UNUSED0) != DIO_INPUT ||
-            getDioDirection(_HCR_DIN_UNUSED1) != DIO_INPUT ||
-            getDioDirection(_HCR_DIN_UNUSED2) != DIO_INPUT ||
-            getDioDirection(_HCR_DIN_UNUSED3) != DIO_INPUT ||
-            getDioDirection(_HCR_DIN_SCM_STATUS0) != DIO_INPUT ||
-            getDioDirection(_HCR_DIN_SCM_STATUS1) != DIO_INPUT ||
-            getDioDirection(_HCR_DIN_SCM_STATUS2) != DIO_INPUT ||
-            getDioDirection(_HCR_DIN_SCM_STATUS3) != DIO_INPUT) {
+    if (getDioDirection(_HCR_DIN_WG_SWITCH_A) != DIO_INPUT ||
+            getDioDirection(_HCR_DIN_WG_SWITCH_B) != DIO_INPUT ||
+            getDioDirection(_HCR_DIN_15_5GHZ_PHASELOCK) != DIO_INPUT ||
+            getDioDirection(_HCR_DIN_1250MHZ_PHASELOCK) != DIO_INPUT ||
+            getDioDirection(_HCR_DIN_HMC_STATUS0) != DIO_INPUT ||
+            getDioDirection(_HCR_DIN_HMC_STATUS1) != DIO_INPUT ||
+            getDioDirection(_HCR_DIN_HMC_MODPULSE_DISABLED) != DIO_INPUT ||
+            getDioDirection(_HCR_DIN_7_UNUSED) != DIO_INPUT) {
         ELOG << __PRETTY_FUNCTION__ << ": Hcr PMC-730 DIO lines " <<
-                _HCR_DIN_UNUSED0 << ", " << _HCR_DIN_UNUSED1 << ", " << 
-                _HCR_DIN_UNUSED2 << ", " << _HCR_DIN_UNUSED3 << ", " <<
-                _HCR_DIN_SCM_STATUS0 << ", " << _HCR_DIN_SCM_STATUS1 << ", " <<
-                _HCR_DIN_SCM_STATUS2 << ", and " << _HCR_DIN_SCM_STATUS3 <<
-                " are not all set for input!";
+            _HCR_DIN_WG_SWITCH_A << ", " << _HCR_DIN_WG_SWITCH_B << ", " << 
+            _HCR_DIN_15_5GHZ_PHASELOCK << ", " << 
+            _HCR_DIN_1250MHZ_PHASELOCK << ", " <<
+            _HCR_DIN_HMC_STATUS0 << ", " << _HCR_DIN_HMC_STATUS1 << ", " <<
+            _HCR_DIN_HMC_MODPULSE_DISABLED << ", and " << _HCR_DIN_7_UNUSED <<
+            " are not all set for input!";
         abort();
     }
     // Verify that our defined output DIO lines are all actually set for output
     if (getDioDirection(_HCR_DOUT_NOISE_SRC_ON) != DIO_OUTPUT ||
-            getDioDirection(_HCR_DOUT_SCM_RESET) != DIO_OUTPUT ||
+            getDioDirection(_HCR_DOUT_HMC_RESET) != DIO_OUTPUT ||
             getDioDirection(_HCR_DOUT_WG_SWITCH_D) != DIO_OUTPUT ||
             getDioDirection(_HCR_DOUT_WG_SWITCH_C) != DIO_OUTPUT ||
-            getDioDirection(_HCR_DOUT_TX_FILAMENT_ON_P) != DIO_OUTPUT ||
-            getDioDirection(_HCR_DOUT_TX_FILAMENT_ON_N) != DIO_OUTPUT ||
-            getDioDirection(_HCR_DOUT_TX_HV_ON_P) != DIO_OUTPUT ||
-            getDioDirection(_HCR_DOUT_TX_HV_ON_N) != DIO_OUTPUT) {
+            getDioDirection(_HCR_DOUT_TX_FILAMENT_ON) != DIO_OUTPUT ||
+            getDioDirection(_HCR_DOUT_HMC_OPS_MODE_BIT0) != DIO_OUTPUT ||
+            getDioDirection(_HCR_DOUT_TX_HV_ON) != DIO_OUTPUT ||
+            getDioDirection(_HCR_DOUT_HMC_OPS_MODE_BIT1) != DIO_OUTPUT) {
         ELOG << __PRETTY_FUNCTION__ << ": Hcr PMC-730 DIO lines " <<
-                _HCR_DOUT_NOISE_SRC_ON << ", " << _HCR_DOUT_SCM_RESET << ", " <<
-                _HCR_DOUT_WG_SWITCH_D << ", " << _HCR_DOUT_WG_SWITCH_C << ", " <<
-                _HCR_DOUT_TX_FILAMENT_ON_P << ", " << 
-                _HCR_DOUT_TX_FILAMENT_ON_N << ", " <<
-                _HCR_DOUT_TX_HV_ON_P << ", and " << _HCR_DOUT_TX_HV_ON_N << 
-                " are not all set for output!";
+            _HCR_DOUT_NOISE_SRC_ON << ", " << _HCR_DOUT_HMC_RESET << ", " <<
+            _HCR_DOUT_WG_SWITCH_D << ", " << _HCR_DOUT_WG_SWITCH_C << ", " <<
+            _HCR_DOUT_TX_FILAMENT_ON << ", " << 
+            _HCR_DOUT_HMC_OPS_MODE_BIT0 << ", " <<
+            _HCR_DOUT_TX_HV_ON << ", and " << _HCR_DOUT_HMC_OPS_MODE_BIT1 << 
+            " are not all set for output!";
         abort();
     }
 }
@@ -95,24 +90,4 @@ HcrPmc730::theHcrPmc730() {
         _theHcrPmc730 = new HcrPmc730();
     }
     return(*_theHcrPmc730);
-}
-
-void
-HcrPmc730::_sendDifferentialSignal(bool signalHigh, DoutLine_t dioPosLine,
-        DoutLine_t dioNegLine) {
-    // as written, this only works for DIO lines in the range 8-15!
-    assert(dioPosLine >=8 && dioPosLine <= 15 &&
-            dioNegLine >= 8 && dioNegLine <= 15);
-    // start from the current state of lines 8-15
-    uint8_t new8_15 = getDio8_15();
-    // change the two lines we care about
-    if (signalHigh) {
-        new8_15 = _turnBitOn(new8_15, dioPosLine - 8);
-        new8_15 = _turnBitOff(new8_15, dioNegLine - 8);
-    } else {
-        new8_15 = _turnBitOff(new8_15, dioPosLine - 8);
-        new8_15 = _turnBitOn(new8_15, dioNegLine - 8);
-    }
-    // ship out the new state
-    setDio8_15(new8_15);
 }
--- src/hcrdrx/HcrPmc730.h
+++ src/hcrdrx/HcrPmc730.h
@@ -14,11 +14,14 @@
 /// HCR DRX machine. 
 class HcrPmc730 : public Pmc730 {
 public:
-    /// Return a reference to HCR's singleton Pmc730 instance.
+    /** 
+     * @brief Return a reference to HCR's singleton Pmc730 instance.
+     * @return a reference to HCR's singleton Pmc730 instance.
+     */
     static HcrPmc730 & theHcrPmc730();
     
     /**
-     * Static method to change whether the singleton instance will be created
+     * @brief Static method to change whether the singleton instance will be created
      * as a simulated PMC-730. This must be called before the singleton is
      * instantiated.
      * @param simulate If true, the singleton will be created as a simulated
@@ -40,59 +43,48 @@ private:
      * Input DIO lines
      */
     typedef enum {
-        /// DIO line 0 unused
-        _HCR_DIN_UNUSED0 = 0,
-        /// DIO line 1 unused
-        _HCR_DIN_UNUSED1 = 1,
-        /// DIO line 2 unused
-        _HCR_DIN_UNUSED2 = 2,
-        /// DIO line 3 unused
-        _HCR_DIN_UNUSED3 = 3,
-        /// SCM status bit 0
-        _HCR_DIN_SCM_STATUS0 = 4,
-        /// SCM status bit 1
-        _HCR_DIN_SCM_STATUS1 = 5,
-        /// SCM status bit 2
-        _HCR_DIN_SCM_STATUS2 = 6,
-        /// SCM status bit 3
-        _HCR_DIN_SCM_STATUS3 = 7
+        /// digital in line 0: waveguide switch A position
+        _HCR_DIN_WG_SWITCH_A = 0,
+        /// digital in line 1: waveguide switch B position
+        _HCR_DIN_WG_SWITCH_B = 1,
+        /// digital in line 2: 15.5 GHz PLO phase locked
+        _HCR_DIN_15_5GHZ_PHASELOCK = 2,
+        /// digital in line 3: 1250 MHz PLO phase locked
+        _HCR_DIN_1250MHZ_PHASELOCK = 3,
+        /// digital in line 4: HMC status bit 0
+        _HCR_DIN_HMC_STATUS0 = 4,
+        /// digital in line 5: HMC status bit 1
+        _HCR_DIN_HMC_STATUS1 = 5,
+        /// digital in line 6: HMC modulation pulse disabled
+        _HCR_DIN_HMC_MODPULSE_DISABLED = 6,
+        /// digital in line 7: unused
+        _HCR_DIN_7_UNUSED = 7
     } DinLine_t;
     
     /**
      * Output DIO lines
      */
     typedef enum {
-        /// Turn on noise source
+        /// digital out line 8: Turn on noise source
         _HCR_DOUT_NOISE_SRC_ON = 8,
-        /// SCM reset signal
-        _HCR_DOUT_SCM_RESET = 9,
-        /// waveguide switch 'D' control
+        /// digital out line 9: SCM reset signal
+        _HCR_DOUT_HMC_RESET = 9,
+        /// digital out line 10: waveguide switch D control
         _HCR_DOUT_WG_SWITCH_D = 10,
-        /// waveguide switch 'C' control
+        /// digital out line 11: waveguide switch C control
         _HCR_DOUT_WG_SWITCH_C = 11,
-        /// Turn on transmitter Klystron filament (differential +)
-        _HCR_DOUT_TX_FILAMENT_ON_P = 12,
-        /// Turn on transmitter Klystron filament (differential -)
-        _HCR_DOUT_TX_FILAMENT_ON_N = 13,
-        /// Turn on transmitter Klystron high voltage (differential +)
-        _HCR_DOUT_TX_HV_ON_P = 14,
-        /// Turn on transmitter Klystron high voltage (differential -)
-        _HCR_DOUT_TX_HV_ON_N = 15,
+        /// digital out line 12: Turn on transmitter Klystron filament
+        _HCR_DOUT_TX_FILAMENT_ON = 12,
+        /// digital out line 13: HMC operation mode bit 0
+        _HCR_DOUT_HMC_OPS_MODE_BIT0 = 13,
+        /// digital out line 14: Turn on transmitter Klystron high voltage
+        _HCR_DOUT_TX_HV_ON = 14,
+        /// digital out line 15: HMC operation mode bit 1
+        _HCR_DOUT_HMC_OPS_MODE_BIT1 = 15,
     } DoutLine_t;
     
     /**
-     * Send a differential signal over two selected PMC730 DIO lines.
-     * Line dioPosLine will get the positive signal, and line dioNegLine will
-     * get the inverted signal.
-     * @param signalHigh if true, a high signal will be sent, otherwise low
-     * @param dioPosLine the output line for the positive signal
-     * @param dioNegLine the output line for the inverted signal
-     */
-    void _sendDifferentialSignal(bool signalHigh, DoutLine_t dioPosLine,
-            DoutLine_t dioNegLine);
-    
-    /**
-     * Set the selected bit in the source byte and return the result.
+     * @brief Set the selected bit in the source byte and return the result.
      * @param src the source byte
      * @param bitnum the number of the bit to set; 0 for least significant bit
      * @return a byte copied from src, but with the selected bit set
@@ -104,7 +96,7 @@ private:
     }
 
     /**
-     * Unset the selected bit in the source byte and return the result.
+     * @brief Unset the selected bit in the source byte and return the result.
      * @param src the source byte
      * @param bitnum the number of the bit to unset; 0 for least significant bit
      * @return a byte copied from src, but with the selected bit unset
@@ -115,11 +107,10 @@ private:
         return(src & ~mask);
     }
     
+    /**
+     * @brief The singleton instance of HcrPmc730.
+     */
     static HcrPmc730 * _theHcrPmc730;
-
-    // Keep the time of day of instantiation (in seconds). This allows us to
-    // return a reasonable pulse count if we're simulating.
-    double _startTimeOfDay;
 };
 
 #endif /* HCRPMC730_H_ */
