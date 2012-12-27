b17ac67c68c141b68260330640894a9494604bf0
Chris Burghart <burghart@ucar.edu>
2012-12-27 22:12:46 +0000

    o instantiate a CmigitsSharedMemory instance with read/write access and cause it to be filled with latest data from our Cmigits instance
    o handle interrupt signals so that we can shut down cleanly on ^C
    
    git-svn-id: http://svn/svn/hcr/trunk@734 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/cmigits/cmigitsDaemon.cpp
+++ src/cmigits/cmigitsDaemon.cpp
@@ -6,18 +6,32 @@
  */
 #include <iostream>
 #include <unistd.h>
+#include <csignal>
 #include <QApplication>
 #include <logx/Logging.h>
 
 LOGGING("cmigitsDaemon")
 
 #include "Cmigits.h"
+#include "CmigitsSharedMemory.h"
 
 quint16 ServerPort = 8001;
 
+QCoreApplication * App = 0;
+bool Interrupted = false;
+
+void
+sigHandler(int signal) {
+    App->quit();
+}
+
+
 int
 main(int argc, char *argv[]) {
-    QCoreApplication app(argc, argv);
+    App = new QCoreApplication(argc, argv);
+
+    // Catch interrupt signal to shut us down cleanly on ^C
+    signal(SIGINT, sigHandler);
     
     // Let logx get and strip out its arguments
     logx::ParseLogArgs(argc, argv);
@@ -31,6 +45,20 @@ main(int argc, char *argv[]) {
     std::string devName(argv[1]);
     Cmigits cm(devName);
 
-    app.exec();
+    CmigitsSharedMemory shm(QSharedMemory::ReadWrite);
+    // When new status arrives, stuff into shared memory.
+    QObject::connect(
+            &cm, SIGNAL(newStatusData(uint64_t, uint16_t, bool, bool, uint16_t, uint16_t, uint16_t, uint16_t, float, float, float)),
+            &shm, SLOT(setLatestStatus(uint64_t, uint16_t, bool, bool, uint16_t, uint16_t, uint16_t, uint16_t, float, float, float)));
+    // When new navigation solution arrives, stuff into shared memory.
+    QObject::connect(
+            &cm, SIGNAL(new3501Data(uint64_t, float, float, float, float, float, float)),
+            &shm, SLOT(setLatestNavSolution(uint64_t, float, float, float, float, float, float)));
+    // When new attitude arrives, stuff into shared memory.
+    QObject::connect(
+            &cm, SIGNAL(new3512Data(uint64_t, float, float, float)),
+            &shm, SLOT(setLatestAttitudes(uint64_t, float, float, float)));
+
+    App->exec();
     return 0;
 }
