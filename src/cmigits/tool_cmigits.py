cdacec1dd7a2a98f670127a2b4d07059ef5e16cf
Chris Burghart <burghart@ucar.edu>
2012-12-31 20:54:21 +0000

    Add a separate SConscript to build cmigitsDaemon, and tweak tool_cmigits.py a bit. The cmigits tool still is not working correctly.
    git-svn-id: http://svn/svn/hcr/trunk@748 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/cmigits/tool_cmigits.py
+++ src/cmigits/tool_cmigits.py
@@ -1,41 +1,29 @@
 #
 # Rules to build libcmigits.a and export it as a SCons tool
 #
-
 tools = ['doxygen', 'logx', 'qt4']
-env = Environment(tools=['default'] + tools)
-env.EnableQt4Modules(['QtCore', 'QtGui', 'QtNetwork'])
-
-
-tooldir = env.Dir('.').srcnode().abspath    # this directory
-
-# The library and header files will live in this directory.
-libDir = tooldir
-includeDir = tooldir
-
-sources = Split('''
-    cmigitsDaemon.cpp
-    Cmigits.cpp
-    CmigitsSharedMemory.cpp
-''')
-
-headers = Split('''
-    Cmigits.h
-    CmigitsSharedMemory.h
-''')
-
-lib = env.Library('cmigits', sources)
-Default(lib)
-
-daemon = env.Program('cmigitsDaemon', sources)
-Default(daemon)
-
-html = env.Apidocs(sources + headers, DOXYFILE_DICT={'PROJECT_NAME':'cmigits'})
-Default(html)
+qt4modules = ['QtGui']
+cmigitsDir = Dir('.').abspath
 
 def cmigits(env):
-    env.AppendUnique(CPPPATH = [includeDir])
-    env.AppendUnique(LIBS = [lib])
+#    env.Append(LIBS=[env.GetGlobalTarget('libcmigits'),])
+    env.AppendUnique(CPPPATH = cmigitsDir)
+    env.AppendDoxref(doxref[0])
     env.Require(tools)
+    env.EnableQt4Modules(qt4modules)
 
 Export('cmigits')
+
+
+env = Environment(tools=['default'] + tools)
+env.EnableQt4Modules(qt4modules)
+
+sources = ['Cmigits.cpp', 'CmigitsSharedMemory.cpp']
+
+headers = ['Cmigits.h',  'CmigitsSharedMemory.h']
+
+cmlib = env.Library('cmigits', sources)
+Default(cmlib)
+
+env['DOXYFILE_DICT'].update({ "PROJECT_NAME" : "cmigits library" })
+doxref = env.Apidocs(sources + headers)
