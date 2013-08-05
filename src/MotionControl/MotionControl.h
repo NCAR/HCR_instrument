64c046394be22250cde74d53e2fdbf8160801b77
Chris Burghart <burghart@ucar.edu>
2013-08-05 15:03:51 +0000

    Indentation fix (get rid of tabs).
    git-svn-id: http://svn/svn/hcr/trunk@933 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/MotionControl/MotionControl.h
+++ src/MotionControl/MotionControl.h
@@ -17,199 +17,199 @@
 
 class MotionControl : public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	/// @brief Constructor
-	MotionControl();
-	/// @brief Destructor
-	virtual ~MotionControl();
+    /// @brief Constructor
+    MotionControl();
+    /// @brief Destructor
+    virtual ~MotionControl();
 
     /// @brief Point the antenna to a specific angle
     /// @param angle The angle that the antenna points to
-	void point(float angle);
+    void point(float angle);
 
-	/// @brief Scan the antenna between the given counterclockwise and clockwise
-	/// limits, at the given scan rate.
-	/// @param ccwLimit the counterclockwise limit of the scan, deg
-	/// @param cwLimit the clockwise limit of the scan, deg
-	/// @pararm scanRate the scan rate, deg/s
-	void scan(float ccwLimit, float cwLimit, float scanRate);
+    /// @brief Scan the antenna between the given counterclockwise and clockwise
+    /// limits, at the given scan rate.
+    /// @param ccwLimit the counterclockwise limit of the scan, deg
+    /// @param cwLimit the clockwise limit of the scan, deg
+    /// @pararm scanRate the scan rate, deg/s
+    void scan(float ccwLimit, float cwLimit, float scanRate);
 
-	/// @brief Get current aircraft attitude and adjust drive position.
-	/// This method does nothing when attitude correction is disabled.
-	void correctForAttitude();
+    /// @brief Get current aircraft attitude and adjust drive position.
+    /// This method does nothing when attitude correction is disabled.
+    void correctForAttitude();
 
-	/// @brief Return a reference to the rotation drive.
-	/// @return a reference to the rotation drive.
-	const ElmoServoDrive & rotationDrive() const { return(_rotDrive); }
+    /// @brief Return a reference to the rotation drive.
+    /// @return a reference to the rotation drive.
+    const ElmoServoDrive & rotationDrive() const { return(_rotDrive); }
 
-	/// @brief Return a reference to the tilt drive.
-	/// @return a reference to the tilt drive.
-	const ElmoServoDrive & tiltDrive() const { return(_tiltDrive); }
+    /// @brief Return a reference to the tilt drive.
+    /// @return a reference to the tilt drive.
+    const ElmoServoDrive & tiltDrive() const { return(_tiltDrive); }
 
-	/// Antenna motion mode: pointing or scanning
-	typedef enum {
-		POINTING,
-		SCANNING
-	} AntennaMode;
+    /// Antenna motion mode: pointing or scanning
+    typedef enum {
+        POINTING,
+        SCANNING
+    } AntennaMode;
 
-	/// @brief Return the current antenna mode.
-	/// @return the current antenna mode.
-	AntennaMode antennaMode() const { return(_antennaMode); }
+    /// @brief Return the current antenna mode.
+    /// @return the current antenna mode.
+    AntennaMode antennaMode() const { return(_antennaMode); }
 
-	/// @brief Return the fixed pointing angle, deg.
-	/// @return the fixed pointing angle, deg.
-	float fixedPointingAngle() const { return(_fixedPointingAngle); }
+    /// @brief Return the fixed pointing angle, deg.
+    /// @return the fixed pointing angle, deg.
+    float fixedPointingAngle() const { return(_fixedPointingAngle); }
 
-	/// @brief Return the antenna scanning parameters
-	/// @param[out] ccwLimit, counterclockwise scan limit, deg
-	/// @param[out] cwLimit, clockwise scan limit, deg
-	/// @param[out] scanRate, scan rate, deg/s
-	/// @return the antenna scanning parameters.
-	void getScanParams(float & ccwLimit, float & cwLimit, float & scanRate) const {
-		ccwLimit = _scanCcwLimit;
-		cwLimit = _scanCwLimit;
-		scanRate = _scanRate;
-	}
+    /// @brief Return the antenna scanning parameters
+    /// @param[out] ccwLimit, counterclockwise scan limit, deg
+    /// @param[out] cwLimit, clockwise scan limit, deg
+    /// @param[out] scanRate, scan rate, deg/s
+    /// @return the antenna scanning parameters.
+    void getScanParams(float & ccwLimit, float & cwLimit, float & scanRate) const {
+        ccwLimit = _scanCcwLimit;
+        cwLimit = _scanCwLimit;
+        scanRate = _scanRate;
+    }
 
-	/// @brief Set the state of attitude correction.
-	/// @param enabled true to enable attitude correction, false to disable
-	void setCorrectionEnabled(bool enabled);
+    /// @brief Set the state of attitude correction.
+    /// @param enabled true to enable attitude correction, false to disable
+    void setCorrectionEnabled(bool enabled);
 
-	/// @brief Return true iff attitude correction is enabled.
-	/// @return true iff attitude correction is enabled.
-	bool attitudeCorrectionEnabled() const { return(_attitudeCorrectionEnabled); }
+    /// @brief Return true iff attitude correction is enabled.
+    /// @return true iff attitude correction is enabled.
+    bool attitudeCorrectionEnabled() const { return(_attitudeCorrectionEnabled); }
 
-	/// @brief Simple class to encapsulate status of a MotionControl object.
-	class Status {
-	public:
-		/// @brief Default constructor.
-		Status();
+    /// @brief Simple class to encapsulate status of a MotionControl object.
+    class Status {
+    public:
+        /// @brief Default constructor.
+        Status();
 
-	    /// @brief Construct status using current values from the given
-		/// MotionControl.
-	    Status(const MotionControl & mc);
+        /// @brief Construct status using current values from the given
+        /// MotionControl.
+        Status(const MotionControl & mc);
 
-	    /// @brief Construct from an xmlrpc_c::value_struct dictionary as returned
-	    /// by a call to the Status::to_value_struct() method.
-	    /// @param statusDict an xmlrpc_c::value_struct dictionary as returned by a
-	    /// call to the Status::to_value_struct() method.
-	    Status(xmlrpc_c::value_struct & statusDict);
+        /// @brief Construct from an xmlrpc_c::value_struct dictionary as returned
+        /// by a call to the Status::to_value_struct() method.
+        /// @param statusDict an xmlrpc_c::value_struct dictionary as returned by a
+        /// call to the Status::to_value_struct() method.
+        Status(xmlrpc_c::value_struct & statusDict);
 
-	    /// @brief destructor
-	    virtual ~Status();
+        /// @brief destructor
+        virtual ~Status();
 
-	    /// @brief Return an external representation of the object's state as
-	    /// an xmlrpc_c::value_struct dictionary.
-	    ///
-	    /// The returned value can be used on the other side of an XML-RPC
-	    /// connection to create an identical object via the
-	    /// Status(const xmlrpc_c::value_struct &) constructor.
-	    /// @return an external representation of the object's state as
-	    /// an xmlrpc_c::value_struct dictionary.
-	    xmlrpc_c::value_struct to_value_struct() const;
+        /// @brief Return an external representation of the object's state as
+        /// an xmlrpc_c::value_struct dictionary.
+        ///
+        /// The returned value can be used on the other side of an XML-RPC
+        /// connection to create an identical object via the
+        /// Status(const xmlrpc_c::value_struct &) constructor.
+        /// @return an external representation of the object's state as
+        /// an xmlrpc_c::value_struct dictionary.
+        xmlrpc_c::value_struct to_value_struct() const;
 
-	    /**
-	     * @brief Return antenna scanning parameters.
-	     * @param[out] ccwLimit the counterclockwise scan limit, deg
-	     * @param[out] cwLimit the clockwise scan limit, deg
-	     * @param[out] rate the scan rate, deg/s
-	     */
-	    void scanParameters(float & ccwLimit, float & cwLimit, float & rate) {
-	    	ccwLimit = scanCcwLimit;
-	    	cwLimit = scanCwLimit;
-	    	rate = scanRate;
-	    }
+        /**
+         * @brief Return antenna scanning parameters.
+         * @param[out] ccwLimit the counterclockwise scan limit, deg
+         * @param[out] cwLimit the clockwise scan limit, deg
+         * @param[out] rate the scan rate, deg/s
+         */
+        void scanParameters(float & ccwLimit, float & cwLimit, float & rate) {
+            ccwLimit = scanCcwLimit;
+            cwLimit = scanCwLimit;
+            rate = scanRate;
+        }
 
-	    /// Is the rotation drive responding?
-	    bool rotDriveResponding;
-	    /// Rotation drive status register
-	    ElmoServoDrive::StatusReg rotDriveStatusReg;
-	    /// Rotation drive temperature, deg C
-	    int rotDriveTemp;
-	    /// Is the tilt drive responding?
-	    bool tiltDriveResponding;
-	    /// Tilt drive status register
-	    ElmoServoDrive::StatusReg tiltDriveStatusReg;
-	    /// Tilt drive temperature, deg C
-	    int tiltDriveTemp;
-	    /// Antenna motion mode
-	    AntennaMode antennaMode;
-	    // Pointing angle for POINTING mode
-	    float fixedPointingAngle;
-	    // Scanning counterclockwise limit, deg
-	    float scanCcwLimit;
-	    // Scanning clockwise limit, deg
-	    float scanCwLimit;
-	    // Scan rate, deg/s
-	    float scanRate;
-	    // Attitude correction enabled?
-	    bool attitudeCorrectionEnabled;
-	};
+        /// Is the rotation drive responding?
+        bool rotDriveResponding;
+        /// Rotation drive status register
+        ElmoServoDrive::StatusReg rotDriveStatusReg;
+        /// Rotation drive temperature, deg C
+        int rotDriveTemp;
+        /// Is the tilt drive responding?
+        bool tiltDriveResponding;
+        /// Tilt drive status register
+        ElmoServoDrive::StatusReg tiltDriveStatusReg;
+        /// Tilt drive temperature, deg C
+        int tiltDriveTemp;
+        /// Antenna motion mode
+        AntennaMode antennaMode;
+        // Pointing angle for POINTING mode
+        float fixedPointingAngle;
+        // Scanning counterclockwise limit, deg
+        float scanCcwLimit;
+        // Scanning clockwise limit, deg
+        float scanCwLimit;
+        // Scan rate, deg/s
+        float scanRate;
+        // Attitude correction enabled?
+        bool attitudeCorrectionEnabled;
+    };
 
-	/// @brief Return a MotionControl::Status object containing current
-	/// status.
-	/// @return a MotionControl::Status object containing current status.
-	Status status() const { return(Status(*this)); }
+    /// @brief Return a MotionControl::Status object containing current
+    /// status.
+    /// @return a MotionControl::Status object containing current status.
+    Status status() const { return(Status(*this)); }
 
 private:
-	// Given desired rotation and tilt angles, adjust to pod-relative angles
-	// using the given pod attitude information.
-	// @param[in,out] rot the desired rotation angle (in), and the needed
-	//	 pod-relative rotation angle (out), deg
-	// @param[in,out] tilt the desired tilt angle (in), and the needed
-	//   pod-relative tilt angle (out), deg
-	// @param pitch the pod pitch, deg
-	// @param roll the pod roll angle, deg
-	// @param drift the pod drift angle, deg
-	void _adjustForAttitude(float & rot, float & tilt, float pitch, float roll,
-			float drift);
+    // Given desired rotation and tilt angles, adjust to pod-relative angles
+    // using the given pod attitude information.
+    // @param[in,out] rot the desired rotation angle (in), and the needed
+    //	 pod-relative rotation angle (out), deg
+    // @param[in,out] tilt the desired tilt angle (in), and the needed
+    //   pod-relative tilt angle (out), deg
+    // @param pitch the pod pitch, deg
+    // @param roll the pod roll angle, deg
+    // @param drift the pod drift angle, deg
+    void _adjustForAttitude(float & rot, float & tilt, float pitch, float roll,
+            float drift);
 
-	/// Adjust pointing position according to aircraft attitude
+    /// Adjust pointing position according to aircraft attitude
     /// @param pitch The aircraft pitch angle
     /// @param roll The aircraft roll angle
     /// @param drift The aircraft drift angle
-	void _adjustPointingForAttitude(float pitch, float roll, float drift);
+    void _adjustPointingForAttitude(float pitch, float roll, float drift);
 
-	/// Adjust scanning table according to aircraft attitude
+    /// Adjust scanning table according to aircraft attitude
     /// @param pitch The aircraft pitch angle
     /// @param roll The aircraft roll angle
     /// @param drift The aircraft drift angle
-	void _adjustScanningForAttitude(float pitch, float roll, float drift);
+    void _adjustScanningForAttitude(float pitch, float roll, float drift);
 
-	/// Rotation drive
-	ElmoServoDrive _rotDrive;
-	/// Tilt drive
-	ElmoServoDrive _tiltDrive;
+    /// Rotation drive
+    ElmoServoDrive _rotDrive;
+    /// Tilt drive
+    ElmoServoDrive _tiltDrive;
 
-	/// Current antenna mode
-	AntennaMode _antennaMode;
+    /// Current antenna mode
+    AntennaMode _antennaMode;
 
-	/// Current angle for fixed pointing
-	float _fixedPointingAngle;
+    /// Current angle for fixed pointing
+    float _fixedPointingAngle;
 
-	/// Scanning counterclockwise limit, deg
-	float _scanCcwLimit;
+    /// Scanning counterclockwise limit, deg
+    float _scanCcwLimit;
 
-	/// Scanning clockwise limit, deg
-	float _scanCwLimit;
+    /// Scanning clockwise limit, deg
+    float _scanCwLimit;
 
-	/// Scan rate, deg/s
-	float _scanRate;
+    /// Scan rate, deg/s
+    float _scanRate;
 
-	/// Cmigits shared memory object
-	CmigitsSharedMemory _cmigitsShm;
+    /// Cmigits shared memory object
+    CmigitsSharedMemory _cmigitsShm;
 
-	/// Is attitude correction enabled?
-	bool _attitudeCorrectionEnabled;
+    /// Is attitude correction enabled?
+    bool _attitudeCorrectionEnabled;
 
-	/// Are we generating fake attitude values?
-	bool _fakeAttitude;
+    /// Are we generating fake attitude values?
+    bool _fakeAttitude;
 
-	/// Drive "start" time (object instantiation time), used when generating
-	/// fake attitude.
-	QTime _driveStartTime;
+    /// Drive "start" time (object instantiation time), used when generating
+    /// fake attitude.
+    QTime _driveStartTime;
 };
 
 #endif /* MOTIONCONTROL_H_ */
