05d1b6b787c9815033e415b3337de53cbf842c1d
Chris Burghart <burghart@ucar.edu>
2013-08-09 22:20:32 +0000

    More implementation for HcrPmc730Daemon: compiles, but is still incomplete.
    git-svn-id: http://svn/svn/hcr/branches/pmc730_daemon@954 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/HcrPmc730Daemon/HcrPmc730Daemon.cpp
+++ src/HcrPmc730Daemon/HcrPmc730Daemon.cpp
@@ -1,21 +1,71 @@
 /*
  * HcrPmc730Daemon.cpp
  *
+ * Application provided to open and provide access to HCR's PMC-730 multi-IO
+ * card. Access to card functions and values is provided via XML-RPC methods.
+ *
  *  Created on: Aug 9, 2013
  *      Author: hcr
  */
 #include <csignal>
+#include <sys/time.h>
+#include <xmlrpc-c/registry.hpp>
+#include <xmlrpc-c/server_abyss.hpp>
+
 #include <toolsa/pmu.h>
 #include <logx/Logging.h>
 #include <HcrPmc730.h>
+#include <QCoreApplication>
 
 LOGGING("HcrPmc730Daemon")
 
-std::string _instance = "ops";   ///< application instance name
+/// Application instance name, for procmap
+std::string _instance = "ops";
+
+/// Global boolean set when exit from the application has been requested
+bool ExitRequested = false;
+
+// Handler for SIGINT and SIGTERM signals.
+void
+exitHandler(int signal) {
+    ExitRequested = true;
+}
+
+// Handler for SIGALRM signals.
+void
+alarmHandler(int signal) {
+    // Do nothing. This handler just assures that arrival of the signal doesn't
+    // terminate our process. The intention of the signal is to force the
+    // XML-RPC server runOnce() method to return occasionally, even if no
+    // request comes in.
+    DLOG << "Got itimer ALRM signal";
+}
+
+// Create a periodic ALRM signal to break us out of
+// xmlrpc_c::serverAbyss::runOnce() so we can process Qt stuff.
+void
+startXmlrpcWorkAlarm() {
+    const struct timeval tv = { 0, 100000 }; // 0.1 s
+    const struct itimerval iv = { tv, tv };
+    setitimer(ITIMER_REAL, &iv, 0);
+}
+
+// Stop the alarm which breaks us out of xmlrpc_c::serverAbyss::runOnce()
+// while our server is actually processing an XML-RPC command.
+void
+stopXmlrpcWorkAlarm() {
+    // Stop the periodic timer.
+    const struct timeval tv = { 0, 0 }; // zero time stops the timer
+    const struct itimerval iv = { tv, tv };
+    setitimer(ITIMER_REAL, &iv, 0);
+}
 
 int
 main(int argc, char * argv[]) {
-    // Make sure our HcrPmc730 is created in simulation mode if requested
+    QCoreApplication app(argc, argv);
+    app.setApplicationName("HcrPmc730Daemon");
+
+    // Make sure the HcrPmc730 is instantiated in simulation mode if requested
 //    HcrPmc730::doSimulate(hcrConfig.simulate_pmc730());
     HcrPmc730::doSimulate(false);
 
@@ -27,13 +77,21 @@ main(int argc, char * argv[]) {
     HcrPmc730::setXmitterHvOn(false);
     HcrPmc730::setHmcOperationMode(HcrPmc730::HMC_CORNER_REFLECTOR_CAL);
 
-    // set to ignore SIGPIPE errors which occur when sockets
-    // are broken between client and server
-    signal(SIGPIPE, SIG_IGN);
+    // Create our XML-RPC method registry and server instance
+    PMU_auto_register("instantiating XML-RPC server");
+    xmlrpc_c::registry myRegistry;
+//    myRegistry.addMethod("initializeUsingIwg1", new InitializeUsingIwg1Method);
+    quint16 serverPort = 8003;
+    xmlrpc_c::serverAbyss xmlrpcServer(myRegistry, serverPort);
 
-    // set up registration with procmap if instance is specified
-    if (_instance.size() > 0) {
-      PMU_auto_init("HcrPmc730Daemon", _instance.c_str(), PROCMAP_REGISTER_INTERVAL);
-      ILOG << "Will register with procmap as instance: " << _instance;
-    }
+    // Set up an interval timer to deliver SIGALRM every 0.01 s. The signal
+    // arrival causes the XML-RPC server's runOnce() method to return so that
+    // we can process Qt events on a regular basis.
+    signal(SIGALRM, alarmHandler);
+    startXmlrpcWorkAlarm();
+
+    // set up registration with procmap
+    PMU_auto_init("HcrPmc730Daemon", _instance.c_str(), PROCMAP_REGISTER_INTERVAL);
+    ILOG << "HcrPmc730Daemon will register with procmap as instance: " << _instance;
+
 }
