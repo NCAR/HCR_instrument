3d666cddd0c664728c9b7c105f2915cb42253b8c
Chris Burghart <burghart@ucar.edu>
2013-08-14 21:16:19 +0000

    Get status from HcrPmc730Daemon rather than hcrdrx.
    git-svn-id: http://svn/svn/hcr/branches/pmc730_daemon@979 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/hcrgui/hcrgui.cpp
+++ src/hcrgui/hcrgui.cpp
@@ -16,16 +16,16 @@
 LOGGING("hcrgui")
 
 // Default to archiver:8000 for xmitd host and port, and rds:8081 for the
-// hcrdrx host and port.
+// HcrPmc730Daemon host and port.
 const char * DefaultXmitdHostAndPort = "archiver:8000";
-const char * DefaultHcrdrxHostAndPort = "rds:8081";
+const char * DefaultPmc730HostAndPort = "rds:8003";
 const int DefaultCmigitsPort = 8002;
 const int DefaultMotionControlPort = 8080;
 
 void
 usage(const char* appName) {
     ELOG << "Usage: " << appName <<
-            " [<xmitd_host>:<xmitd_port> <hcrdrx_host>:<hcrdrx_port>]";
+            " [<xmitd_host>:<xmitd_port> <pmc730_host>:<pmc730_port>]";
     exit(1);
 }
 
@@ -42,27 +42,27 @@ main(int argc, char *argv[]) {
     // Start with default host/port values, but replace these with argv[1] and
     // argv[2] if they are given.
     const char * xmitdHostAndPort = DefaultXmitdHostAndPort;
-    const char * hcrdrxHostAndPort = DefaultHcrdrxHostAndPort;
+    const char * pmc730HostAndPort = DefaultPmc730HostAndPort;
     if (argc == 3) {
         xmitdHostAndPort = argv[1];
-        hcrdrxHostAndPort = argv[2];
+        pmc730HostAndPort = argv[2];
     }
 
     char xmitdHost[80];
     int xmitdPort;
     char rdsHost[80];
-    int hcrdrxPort;
+    int pmc730Port;
     int cmigitsPort = DefaultCmigitsPort;
     int motionControlPort = DefaultMotionControlPort;
     if (sscanf(xmitdHostAndPort, "%[^:]:%d", xmitdHost, &xmitdPort) != 2) {
         usage(argv[0]);
     }
-    if (sscanf(hcrdrxHostAndPort, "%[^:]:%d", rdsHost, &hcrdrxPort) != 2) {
+    if (sscanf(pmc730HostAndPort, "%[^:]:%d", rdsHost, &pmc730Port) != 2) {
         usage(argv[0]);
     }
 
     QMainWindow* mainWindow = new HcrGuiMainWindow(xmitdHost, xmitdPort,
-            rdsHost, hcrdrxPort, cmigitsPort, motionControlPort);
+            rdsHost, pmc730Port, cmigitsPort, motionControlPort);
     mainWindow->show();
     
     return app->exec();
