a4c7b33b08153453621ff6669b50f17d6876b44b
Chris Burghart <burghart@ucar.edu>
2013-09-02 15:20:11 +0000

    Make sure the special bug-fix instantiation provided here is only applied for Boost 1.41.
    git-svn-id: http://svn/svn/hcr/trunk@1062 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/hcrdrx/XmlRpcValueArchive.cpp
+++ src/hcrdrx/XmlRpcValueArchive.cpp
@@ -9,6 +9,9 @@
 
 #include "XmlRpcValueArchive.h"
 
-// we must explicitly instantiate some implementation for this type of stream
-#include <boost/archive/impl/archive_serializer_map.ipp>
-template class boost::archive::detail::archive_serializer_map<XmlRpcValueIarchive>;
+// Special instantiation required for Boost 1.41
+#if (BOOST_VERSION/100 == 10041)
+   // we must explicitly instantiate some implementation for this type of stream
+#  include <boost/archive/impl/archive_serializer_map.ipp>
+   template class boost::archive::detail::archive_serializer_map<XmlRpcValueIarchive>;
+#endif
