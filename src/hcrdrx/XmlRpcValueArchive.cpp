65123e86c78687c0a8d8ea41a87b84ae046cbbd6
Chris Burghart <burghart@ucar.edu>
2013-09-02 19:08:20 +0000

    Fixed Boost version test.
    git-svn-id: http://svn/svn/hcr/trunk@1063 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/hcrdrx/XmlRpcValueArchive.cpp
+++ src/hcrdrx/XmlRpcValueArchive.cpp
@@ -7,10 +7,11 @@
 
 #define BOOST_ARCHIVE_SOURCE
 
+#include <boost/version.hpp>
 #include "XmlRpcValueArchive.h"
 
 // Special instantiation required for Boost 1.41
-#if (BOOST_VERSION/100 == 10041)
+#if (BOOST_VERSION/100 == 1041)
    // we must explicitly instantiate some implementation for this type of stream
 #  include <boost/archive/impl/archive_serializer_map.ipp>
    template class boost::archive::detail::archive_serializer_map<XmlRpcValueIarchive>;
