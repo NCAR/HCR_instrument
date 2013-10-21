8be71c7913df5e563912a6a8e2616cc2536ee5e0
Chris Burghart <burghart@ucar.edu>
2013-10-21 16:40:16 +0000

    Created a new CmigitsStatusThread class to collect status directly from the cmigitsDaemon via XML-RPC calls.
    git-svn-id: http://svn/svn/hcr/branches/pmc730_daemon@1119 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

---
+++ src/hcrgui/CmigitsStatusThread.h
@@ -0,0 +1,67 @@
+/*
+ * CmigitsStatusThread.h
+ *
+ *  Created on: Sep 24, 2013
+ *      Author: burghart
+ */
+#ifndef CMIGITSSTATUSTHREAD_H_
+#define CMIGITSSTATUSTHREAD_H_
+
+#include <CmigitsDaemonRpcClient.h>
+#include <QThread>
+
+/// @brief Class providing a thread which gets cmigitsDaemon status on a regular
+/// basis using a CmigitsDaemonRpcClient connection.
+///
+/// This class uses the given CmigitsDaemonRpcClient connection to poll for 
+/// status from cmigitsDaemon on a ~1 Hz basis (when connected). When new status
+/// is received, newStatus(CmigitsStatus) signal is emitted. The class also 
+/// provides a useful way to test for good connection to the cmigitsDaemon RPC 
+/// server, via serverResponsive(bool) signals emitted when connection/
+/// disconnection is detected.
+class CmigitsStatusThread : public QThread {
+    Q_OBJECT
+
+public:
+    /// @brief Instantiate, creating an CmigitsDaemonRpcClient connection to the
+    /// cmigitsDaemon XML-RPC server.
+    CmigitsStatusThread(std::string cmigitsHost, int cmigitsPort);
+    virtual ~CmigitsStatusThread();
+
+    void run();
+
+    /// @brief Return the CmigitsDaemonRpcClient instance being used to talk to 
+    /// cmigitsDaemon.
+    /// @return the CmigitsDaemonRpcClient instance being used to talk to 
+    /// cmigitsDaemon.
+    CmigitsDaemonRpcClient & rpcClient() { return *_client; }
+
+signals:
+    /// @brief Signal emitted when the XML-RPC client connection to the server
+    /// becomes responsive or unresponsive.
+    /// @param responsive true if the server has become responsive or false
+    /// if the server has become unresponsive
+    /// @param msg a message describing the associated event
+    void serverResponsive(bool responsive, std::string msg);
+
+    /// @brief signal emitted when a new status is received from CmigitsDaemon
+    /// @param status the new status received from CmigitsDaemon
+    void newStatus(CmigitsStatus status);
+
+private slots:
+    /// @brief Try to get latest status from cmigitsDaemon, and emit a 
+    /// newStatus() signal if successful.
+    void _getStatus();
+private:
+    /// True iff the client had a successful connection with the cmigitsDaemon
+    /// XML-RPC server on the last XML-RPC method call.
+    bool _responsive;
+
+    std::string _cmigitsHost;
+    int _cmigitsPort;
+
+    /// The CmigitsDaemonRpcClient object handling the XML-RPC connection
+    CmigitsDaemonRpcClient * _client;
+};
+
+#endif /* CMIGITSSTATUSTHREAD_H_ */
