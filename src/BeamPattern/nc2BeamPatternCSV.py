7f57b96e180174fd9683bf49b30fd52e6f0742c1
Chris Burghart <burghart@ucar.edu>
2014-01-10 23:21:01 +0000

    Changed the order of arguments, and added the ability to extract from a list of CFRadial files rather than just one file.
    git-svn-id: http://svn/svn/hcr/trunk@1247 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/BeamPattern/nc2BeamPatternCSV.py
+++ src/BeamPattern/nc2BeamPatternCSV.py
@@ -17,41 +17,44 @@ import os
 from netCDF4 import Dataset
 
 if len(sys.argv) < 4:
-    sys.exit("Usage: %s <CFRadial_filename> <dbmVar> <gate>" % sys.argv[0])
+    sys.exit("Usage: %s <dbmVar> <gate> <CFRadial_filename> [...]" % sys.argv[0])
 
-# Make sure the requested file exists
-ncName = sys.argv[1]
-if not os.path.exists(ncName):
-    sys.exit("File %s does not exist" % ncName)
-    
 # Get var name
-varName = sys.argv[2]
+varName = sys.argv[1]
 
 # Parse the gate number 
 gateNum = None
 try:
-    gateNum = int(sys.argv[3])
+    gateNum = int(sys.argv[2])
 except ValueError:
-    sys.exit("Bad gate number '%s'" % sys.argv[3])
+    sys.exit("Bad gate number '%s'" % sys.argv[2])
 
-# Open the netCDF file
-dataset = Dataset(ncName, 'r')
-
-# Extract the start time string and turn it into an integer time stamp in
-# seconds since 1970-01-01 00:00:00 UTC
-startString = ""
-for char in dataset.variables['time_coverage_start'][:]:
-    if not char is numpy.ma.masked:
-        startString += char
-        
-startTime = calendar.timegm(dateParser.parse(startString).utctimetuple())
-time = dataset.variables['time'][:]
-time += startTime
-rot = dataset.variables['rotation'][:]
-tilt = dataset.variables['tilt'][:]
-az = dataset.variables['azimuth'][:]
-el = dataset.variables['elevation'][:]
-dbmValues = dataset.variables[varName][:, gateNum]
- 
-for n in range(len(dbmValues)):
-    print("%s,%s,%s,%s,%s,%s" % (time[n], dbmValues[n], rot[n], tilt[n], az[n], el[n]))
+# For each netCDF file given...
+for n in range(3, len(sys.argv)):
+    # Make sure the requested file exists
+    ncName = sys.argv[n]
+    if not os.path.exists(ncName):
+        sys.exit("File %s does not exist" % ncName)
+     
+    # Open the netCDF file
+    sys.stderr.write("Extracting from %s\n" % ncName)
+    dataset = Dataset(ncName, 'r')
+     
+    # Extract the start time string and turn it into an integer time stamp in
+    # seconds since 1970-01-01 00:00:00 UTC
+    startString = ""
+    for char in dataset.variables['time_coverage_start'][:]:
+        if not char is numpy.ma.masked:
+            startString += char
+             
+    startTime = calendar.timegm(dateParser.parse(startString).utctimetuple())
+    time = dataset.variables['time'][:]
+    time += startTime
+    rot = dataset.variables['rotation'][:]
+    tilt = dataset.variables['tilt'][:]
+    az = dataset.variables['azimuth'][:]
+    el = dataset.variables['elevation'][:]
+    dbmValues = dataset.variables[varName][:, gateNum]
+      
+    for t in range(len(dbmValues)):
+        print("%s,%s,%s,%s,%s,%s" % (time[t], dbmValues[t], rot[t], tilt[t], az[t], el[t]))
