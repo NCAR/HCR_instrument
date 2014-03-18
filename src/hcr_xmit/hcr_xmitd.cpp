ad678037ef32b070f35247d6b302882852917c02
Chris Burghart <burghart@ucar.edu>
2014-03-18 18:56:26 +0000

    comment typo
    git-svn-id: http://svn/svn/hcr/trunk@1310 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/hcr_xmit/hcr_xmitd.cpp
+++ src/hcr_xmit/hcr_xmitd.cpp
@@ -286,7 +286,7 @@ main(int argc, char *argv[]) {
             &updateFuncWrapper, SLOT(callFunction()));
     registerTimer.start();
 
-    // Now just run the application until sombody or something interrupts it
+    // Now just run the application until somebody or something interrupts it
     App->exec();
 
     // Clean up before exit
