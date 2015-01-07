8f152a8f3c2881329fb8d4af8f8dc1bb5b537814
Chris Burghart <burghart@ucar.edu>
2015-01-07 16:53:24 +0000

    In replicating the CmigitsFmqThreadWorker's signals in CmigitsFmqThread, use Qt::DirectConnection so that the signals are emitted from the worker's affinity thread.
    git-svn-id: http://svn/svn/hcr/trunk@1658 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/cmigits/CmigitsFmqThread.cpp
+++ src/cmigits/CmigitsFmqThread.cpp
@@ -17,11 +17,17 @@ CmigitsFmqThread::~CmigitsFmqThread() {
 void
 CmigitsFmqThread::run()
 {
-    // Create our worker and connect its signals to generate our signals.
+    // Create our worker
     CmigitsFmqThreadWorker worker(_minDataIntervalMs, _dataTimeoutMs);
+    
+    // Replicate the worker's signals as our own. Use DirectConnection so that 
+    // our signals are emitted from *this* thread rather than from the thread
+    // where this QThread instance has its affinity.
     connect(&worker, SIGNAL(newData(CmigitsFmq::MsgStruct)),
-            this, SIGNAL(newData(CmigitsFmq::MsgStruct)));
-    connect(&worker, SIGNAL(dataTimeout()), this, SIGNAL(dataTimeout()));
+            this, SIGNAL(newData(CmigitsFmq::MsgStruct)),
+            Qt::DirectConnection);
+    connect(&worker, SIGNAL(dataTimeout()), this, SIGNAL(dataTimeout()),
+            Qt::DirectConnection);
 
     // Now just fire up the event loop.
     exec();
