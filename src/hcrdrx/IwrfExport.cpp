259654ee74c44dcb8b83668dfbbb67f5745abcc7
Chris Burghart <burghart@ucar.edu>
2015-01-13 16:10:49 +0000

    Removed an unused variable.
    git-svn-id: http://svn/svn/hcr/trunk@1678 2e3f476d-dfe7-4101-bf40-6ec35b1080dd

--- src/hcrdrx/IwrfExport.cpp
+++ src/hcrdrx/IwrfExport.cpp
@@ -20,10 +20,6 @@
 
 using namespace std;
 
-// Time out the latest entry in the _cmigitsDeque when it is more than
-// CMIGITS_TIMEOUT_MSECS old.
-static const uint32_t CMIGITS_TIMEOUT_MSECS = 1000;
-
 LOGGING("IwrfExport")
 
 ///////////////////////////////////////////////////////////////////////////
