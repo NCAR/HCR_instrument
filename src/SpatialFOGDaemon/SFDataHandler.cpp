9e053d0ae46c29d2b3ca5858177396a38a8a66d4
Chris Burghart <burghart@ucar.edu>
2016-07-07 21:13:32 +0000

    Add construction of packets to SFDataHandler

--- src/SpatialFOGDaemon/SFDataHandler.cpp
+++ src/SpatialFOGDaemon/SFDataHandler.cpp
@@ -6,18 +6,62 @@
  */
 
 #include <inttypes.h>
+#include <iomanip>
 #include <ANPPPacketFactory.h>
+#include <logx/Logging.h>
 #include "SFDataHandler.h"
 
-SFDataHandler::SFDataHandler() {
+LOGGING("SFDataHandler")
+
+SFDataHandler::SFDataHandler() : _data(), _nskipped(0) {
 }
 
 SFDataHandler::~SFDataHandler() {
 }
 
 void
-SFDataHandler::handleData(QByteArray data) {
-    const uint8_t* uint8Data = reinterpret_cast<uint8_t*>(data.data());
-    int dataLen = data.length();
-    ANPPPacketFactory::instance().constructANPPPacket(uint8Data, dataLen);
+SFDataHandler::handleData(QByteArray newData) {
+    DLOG << "handleData() got " << newData.length() << " bytes";
+
+    // Append the new data to what we already have
+    _data += newData;
+
+    // Look for and pass on any valid packet(s) we now have in the data
+    _parseData();
+}
+
+void
+SFDataHandler::_parseData() {
+    // Try to extract valid ANPP packets from the data
+    while (_data.length() > 0) {
+        // Cast our data into a pointer to an array of uint8_t
+        const uint8_t* uint8Data = reinterpret_cast<uint8_t*>(_data.data());
+        uint dataLen(_data.length());
+
+        try {
+            ANPPPacket * pkt =
+                    ANPPPacketFactory::instance().constructANPPPacket(uint8Data, dataLen);
+            if (_nskipped) {
+                WLOG << "Skipped " << _nskipped << " bytes to find a good header";
+                _nskipped = 0;
+            }
+            ILOG << "Got an ANPP packet with ID " << pkt->packetId() <<
+                    ", CRC is " << (pkt->crcIsGood() ? "good" : "BAD");
+            // Drop the bytes for the packet we just extracted
+            _data = _data.right(_data.length() - pkt->fullPacketLen());
+            emit(SIGNAL(newPacket(*pkt)));
+            delete(pkt);
+            continue;
+        } catch (ANPPPacket::NeedMoreData & x) {
+            // Break out, since we need more data to continue
+            DLOG << "need more data...";
+            break;
+        } catch (ANPPPacket::BadHeader & x) {
+            // Not a valid header. Drop the first byte of _data and try again.
+            WLOG << "Skipping a byte: " << x.what();
+            _data = _data.right(_data.length() - 1);
+            _nskipped++;
+            continue;
+        }
+    }
 }
--- src/SpatialFOGDaemon/SFDataHandler.h
+++ src/SpatialFOGDaemon/SFDataHandler.h
@@ -8,14 +8,45 @@
 #ifndef _SFDATAHANDLER_H_
 #define _SFDATAHANDLER_H_
 
-#include <QtCore/qobject.h>
+#include <vector>
+#include <QObject>
 
+#include <ANPPPacket.h>
+
+/// @brief Handler for incoming data from a Spatial FOG.
+///
+/// This class is passed raw bytes arriving from the Spatial FOG to its
+/// handleData() slot, and emits parsed complete packets via its newPacket()
+/// signal.
 class SFDataHandler: public QObject {
+    Q_OBJECT
 public:
     SFDataHandler();
     virtual ~SFDataHandler();
+
 public slots:
-    void handleData(QByteArray data);
+    /// @brief Slot to accept incoming data from the Spatial FOG and emit
+    /// newPacket() signals as packets can be parsed from the stream.
+    void handleData(QByteArray newData);
+
+signals:
+    /// @brief Signal emitted when a new ANPP packet has arrived from the
+    /// Spatial FOG.
+    void newPacket(ANPPPacket packet);
+
+private:
+    /// @brief Parse the _data array to find complete ANPP packets, and
+    /// emit a newPacket() signal as each new packet is successfully parsed.
+    ///
+    /// Parsed data is removed from the _data array.
+    void _parseData();
+
+    /// @brief Byte array to hold unconsumed data which has come from the
+    /// Spatial FOG.
+    QByteArray _data;
+
+    // Number of bytes skipped to find a valid packet header
+    uint _nskipped;
 };
 
 #endif /* _SFDATAHANDLER_H_ */
