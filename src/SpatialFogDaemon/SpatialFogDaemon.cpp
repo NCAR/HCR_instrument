9966d806db7e99740823c7777d83830a13c7e50b
Chris Burghart <burghart@ucar.edu>
2016-08-09 22:59:53 +0000

    Add PMU registration and an XML-RPC server
    The XML-RPC getStatus() method still needs to be fleshed out

--- src/SpatialFogDaemon/SpatialFogDaemon.cpp
+++ src/SpatialFogDaemon/SpatialFogDaemon.cpp
@@ -1,3 +1,26 @@
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
+// ** Copyright UCAR (c) 1990 - 2016                                         
+// ** University Corporation for Atmospheric Research (UCAR)                 
+// ** National Center for Atmospheric Research (NCAR)                        
+// ** Boulder, Colorado, USA                                                 
+// ** BSD licence applies - redistribution and use in source and binary      
+// ** forms, with or without modification, are permitted provided that       
+// ** the following conditions are met:                                      
+// ** 1) If the software is modified to produce derivative works,            
+// ** such modified software should be clearly marked, so as not             
+// ** to confuse it with the version available from UCAR.                    
+// ** 2) Redistributions of source code must retain the above copyright      
+// ** notice, this list of conditions and the following disclaimer.          
+// ** 3) Redistributions in binary form must reproduce the above copyright   
+// ** notice, this list of conditions and the following disclaimer in the    
+// ** documentation and/or other materials provided with the distribution.   
+// ** 4) Neither the name of UCAR nor the names of its contributors,         
+// ** if any, may be used to endorse or promote products derived from        
+// ** this software without specific prior written permission.               
+// ** DISCLAIMER: THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS  
+// ** OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED      
+// ** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.    
+// *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* 
 /*
  * SpatialFogDaemon.cpp
  *
@@ -9,10 +32,14 @@
 #include <cstring>
 #include <string>
 #include <QFunctionWrapper.h>
+#include <QXmlRpcServerAbyss.h>
 #include <logx/Logging.h>
+#include <toolsa/pmu.h>
 
 #include <QApplication>
 #include <QTimer>
+
+#include <HcrPortNumbers.h>
 
 #include "HcrSpatialFog.h"
 
@@ -20,6 +47,9 @@ LOGGING("SpatialFogDaemon")
 
 // QApplication instance
 QApplication * App;
+
+// Our instance name for PMU
+static const std::string PmuInstance("ops");
 
 void
 usage() {
@@ -34,6 +64,28 @@ void stopApp(int sig) {
         App->quit();
     }
 }
+
+void
+updatePMURegistration() {
+    // Make sure we remain registered with PMU, so it knows we're alive
+    PMU_auto_register("running");
+}
+
+/// XML-RPC method class for getStatus()
+class GetStatusMethod : public xmlrpc_c::method {
+public:
+    GetStatusMethod() {
+        this->_signature = "S:";
+        this->_help = "Return the latest status/data from the SpatialFogDaemon.";
+    }
+    void
+    execute(const xmlrpc_c::paramList & paramList, xmlrpc_c::value* retvalP) {
+        DLOG << "Executing XML-RPC call to getStatus()";
+        // Get the latest status from the FMQ, and convert it to 
+        // an xmlrpc_c::value_struct dictionary.
+//        *retvalP = .toXmlRpcValue();
+    }
+};
 
 int
 main(int argc, char * argv[]) {
@@ -58,10 +110,32 @@ main(int argc, char * argv[]) {
 
     std::string devName(argv[1]);
 
-    // Create an HcrSpatialFog instance, which will open and read the given
+    // Create a HcrSpatialFog instance, which will open and read the given
     // device and publish data from the device on a shared memory file message
     // queue (FMQ).
     HcrSpatialFog * spatialFog = new HcrSpatialFog(devName);
+
+    // set up registration with procmap if instance is specified
+    if (PmuInstance.size() > 0) {
+      PMU_auto_init("SpatialFogDaemon", PmuInstance.c_str(), PROCMAP_REGISTER_INTERVAL);
+      ILOG << "procmap instance '" << PmuInstance << "'";
+    }
+
+    // Create our XML-RPC method registry and server instance
+    PMU_auto_register("instantiating XML-RPC server");
+    xmlrpc_c::registry myRegistry;
+    myRegistry.addMethod("getStatus", new GetStatusMethod);
+    QXmlRpcServerAbyss xmlrpcServer(&myRegistry, SPATIALFOGDAEMON_PORT);
+        
+    // Create a QFunctionWrapper around the updatePMURegistration() function,
+    // as well as a QTimer, and use them to cause a call to the function on
+    // a periodic basis so that PMU knows we're still alive.
+    QFunctionWrapper registrationWrapper(updatePMURegistration);
+    QTimer registrationTimer;
+    registrationTimer.setInterval(10000);   // 10 s
+    QObject::connect(&registrationTimer, SIGNAL(timeout()),
+            &registrationWrapper, SLOT(callFunction()));
+    registrationTimer.start();
 
     // Start the app
     App->exec();
