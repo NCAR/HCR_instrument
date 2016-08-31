8975ab79a06bd85c3208766a5714b065053eb10a
Chris Burghart <burghart@ucar.edu>
2016-08-31 16:07:53 +0000

    Add SpatialFog status, status thread, and RPC client classes

--- src/SpatialFogDaemon/tool_SpatialFogFmq.py
+++ src/SpatialFogDaemon/tool_SpatialFogFmq.py
@@ -2,6 +2,7 @@
 # Rules to build libspatialfogfmq.a and export it as a SCons tool
 #
 tools = Split('''
+    archive_xmlrpc_c
     doxygen
     lrose
     qt4
@@ -12,13 +13,19 @@ env = Environment(tools=['default'] + tools)
 env.EnableQt4Modules(['QtCore'])
 
 sources = Split('''
+    SpatialFogDaemonRpcClient.cpp
     SpatialFogFmq.cpp
     SpatialFogFmqWatcher.cpp
+    SpatialFogStatus.cpp
+    SpatialFogStatusThread.cpp
 ''')
 
 headers = Split('''
+    SpatialFogDaemonRpcClient.h
     SpatialFogFmq.h
     SpatialFogFmqWatcher.h
+    SpatialFogStatus.h
+    SpatialFogStatusThread.h
 ''')
 
 lib = env.Library('SpatialFogFmq', sources)
