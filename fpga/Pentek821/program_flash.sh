828b3ff718b0f232092e764fa19c03e98389460a
karboski <karboski@ucar.edu>
2023-02-03 20:06:24 +0000

    Update path to flash tool

--- fpga/Pentek821/program_flash.sh
+++ fpga/Pentek821/program_flash.sh
@@ -2,5 +2,5 @@
 
 #argument: a folder containing a bitstream
 
-../Navigator_xx821/BSP_2.4/util/flash_util/flash_util.out -b 1 -a -f $1/*.mcs
+/home/hcr/git/HCR_instrument/src/Navigator_xx821/BSP_2.4/util/flash_util/flash_util.out -b 1 -a -f $1/*.mcs
 
